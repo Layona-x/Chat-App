75437b67fae2a42f28d198e1da19609b1ea1b879
Layona-x <109739779+Layona-x@users.noreply.github.com>
2022-07-21 21:00:04 +0300

    Add files via upload

--- models/users.js
+++ models/users.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
+const userSchema = new Schema({
+  username:{
+    type:String,
+    require: true,
+  }
+})
+
+const user = mongoose.model("User", userSchema)
+module.exports = user;
